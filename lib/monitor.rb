99ff59300e9e2b0118030e94874caf63f4503b5e
Andrew Northall <andy@somasonic.org>
2014-04-28 15:51:46 +0100

    .

---
+++ lib/monitor.rb
@@ -0,0 +1,27 @@
+####
+## bnc.im administration bot
+##
+## Copyright (c) 2014 Andrew Northall
+##
+## MIT License
+## See LICENSE file for details.
+####
+
+class MonitorPlugin
+  include Cinch::Plugin
+  
+  listen_to :connect, method: :connect
+  listen_to :disconnect, method: :disconnect
+  
+  def connect(m)
+    adminmsg(Format(:bold, :green, "[CONNECTED] ") + @bot.irc.network.name.to_s)
+  end
+  
+  def connect(m)
+    adminmsg(Format(:bold, :red, "[DISCONNECTED] ") + @bot.irc.network.name.to_s)
+  end
+  
+  def adminmsg(text)
+    $adminbot.irc.send("PRIVMSG #bnc.im-admin :#{text}")
+  end
+end
